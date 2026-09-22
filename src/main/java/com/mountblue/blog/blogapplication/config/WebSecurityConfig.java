fecde2c56293b314e25c16ba3f1945f29e921faf
Amit Singh <as271996@gmail.com>
2026-09-22 17:30:30 +0530

    Switch to NoOpPasswordEncoder for password encoding

--- src/main/java/com/mountblue/blog/blogapplication/config/WebSecurityConfig.java
+++ src/main/java/com/mountblue/blog/blogapplication/config/WebSecurityConfig.java
@@ -7,37 +7,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(getPasswordEncoder());
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                //.antMatchers("/blog/bloglist").hasAnyAuthority("USER", "ADMIN")
                 .antMatchers("/").permitAll()
                 .and()
-                .formLogin()
-                .permitAll()
+                .formLogin().permitAll()
                 .defaultSuccessUrl("/blog/bloglist", true)
                 .and()
-                .logout()
-                .permitAll()
-                .logoutSuccessUrl("/blog/bloglist");
+                .logout().permitAll()
+                .logoutSuccessUrl("/blog/bloglist")
+                ;
+
     }
 }
